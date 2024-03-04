26e6325ba962a216ed8ffb5c92a66bcadd156680
Waldemar17 <Waldemar17@LAB4-3>
2024-03-04 09:11:38 -0600

    se añadio el cargar los datos de las clases y mostrarlos en el gridview

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,63 @@ namespace Lab_reapao__1
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewEmpleados = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
+            this.buttonCargarDatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridViewEmpleados
+            // 
+            this.dataGridViewEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleados.Location = new System.Drawing.Point(43, 33);
+            this.dataGridViewEmpleados.Name = "dataGridViewEmpleados";
+            this.dataGridViewEmpleados.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewEmpleados.TabIndex = 0;
+            // 
+            // dataGridViewAsistencia
+            // 
+            this.dataGridViewAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAsistencia.Location = new System.Drawing.Point(389, 33);
+            this.dataGridViewAsistencia.Name = "dataGridViewAsistencia";
+            this.dataGridViewAsistencia.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewAsistencia.TabIndex = 1;
+            // 
+            // buttonCargarDatos
+            // 
+            this.buttonCargarDatos.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonCargarDatos.Location = new System.Drawing.Point(263, 227);
+            this.buttonCargarDatos.Name = "buttonCargarDatos";
+            this.buttonCargarDatos.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.buttonCargarDatos.Size = new System.Drawing.Size(145, 106);
+            this.buttonCargarDatos.TabIndex = 2;
+            this.buttonCargarDatos.Text = "ver";
+            this.buttonCargarDatos.UseVisualStyleBackColor = true;
+            this.buttonCargarDatos.Click += new System.EventHandler(this.buttonCargarDatos_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCargarDatos);
+            this.Controls.Add(this.dataGridViewAsistencia);
+            this.Controls.Add(this.dataGridViewEmpleados);
             this.Name = "Form1";
             this.Text = "EMPLEADOS";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEmpleados;
+        private System.Windows.Forms.DataGridView dataGridViewAsistencia;
+        private System.Windows.Forms.Button buttonCargarDatos;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,89 @@ namespace Lab_reapao__1
 {
     public partial class Form1 : Form
     {
+        //cargar empleados
+        List<Empleado> empleados = new List<Empleado>();
+        List<Asistencia> asistencias = new List<Asistencia>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void CargarEmpleados()
+        {
+            //Leer el archivo y cargarlo a la lista
+            //no se cargha al Datagridview por las operaciones que debemos realizar
+            string fileName = "Empleados.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                //leer los datos del empleado
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.SueldoHora = Convert.ToDecimal(reader.ReadLine());
+
+                //guardar el empleado en la lista de empleados
+                empleados.Add(empleado);
+            }
+            reader.Close();
+
+        }
+        public void MostrarEmpleados()
+        {
+
+            //Mostrar la lista de empleados en el gridview
+            dataGridViewEmpleados.DataSource = null;
+            dataGridViewEmpleados.DataSource = empleados;
+            dataGridViewEmpleados.Refresh();
+
+        }
+
+        public void CargarAsistencia()
+        {
+            //Leer el archivo y cargarlo a la lista
+            //no se cargha al Datagridview por las operaciones que debemos realizar
+            string fileName = "Asistencia.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                //leer los datos del empleado
+                Asistencia asistencia = new Asistencia();
+                asistencia.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                asistencia.HorasMes = Convert.ToInt16(reader.ReadLine());
+                asistencia.Mes = Convert.ToInt16(reader.ReadLine());
+              
+                asistencias.Add(asistencia);
+            }
+            reader.Close();
+
+        }
+        public void MostrarAsistencia()
+        {
+
+            //Mostrar la lista de empleados en el gridview
+            dataGridViewAsistencia.DataSource = null;
+            dataGridViewAsistencia.DataSource = asistencias;
+            dataGridViewAsistencia.Refresh();
+
+        }
+
+        private void buttonCargarDatos_Click(object sender, EventArgs e)
+        {
+            CargarEmpleados();
+            MostrarEmpleados();
+
+            CargarAsistencia();
+            MostrarAsistencia();
+
+
+
         }
     }
 }
